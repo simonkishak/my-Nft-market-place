be2efa29370361df5acd516978639dd03bb65a8b
Aishat Adenike Akinyemi <52966744+Aishat-Akinyemi@users.noreply.github.com>
2022-06-27 07:24:17 +0100

    Update NFTMKT.sol
    - added and used 2 modifiers canFillOffer() and canCancelOffer()  
    - refactored the claimFunds function :
         -  to prevent reentrancy attacks
         -  also changed the way ether is tranferred to use the **call** method

--- NFTMKT.sol
+++ NFTMKT.sol
@@ -42,25 +42,16 @@ contract NFTMarketplace {
     emit Offer(offerCount, _id, msg.sender, _price, false, false);
   }
 
-  function fillOffer(uint _offerId) public payable {
-    _Offer storage _offer = offers[_offerId];
-    require(_offer.offerId == _offerId, 'The offer must exist');
-    require(_offer.user != msg.sender, 'The owner of the offer cannot fill it');
-    require(!_offer.fulfilled, 'An offer cannot be fulfilled twice');
-    require(!_offer.cancelled, 'A cancelled offer cannot be fulfilled');
-    require(msg.value == _offer.price, 'The ETH amount should match with the NFT Price');
+  function fillOffer(uint _offerId) public payable canFillOffer(_offerId){
+    _Offer storage _offer = offers[_offerId];  
     nftCollection.transferFrom(address(this), msg.sender, _offer.id);
     _offer.fulfilled = true;
     userFunds[_offer.user] += msg.value;
     emit OfferFilled(_offerId, _offer.id, msg.sender);
   }
 
-  function cancelOffer(uint _offerId) public {
-    _Offer storage _offer = offers[_offerId];
-    require(_offer.offerId == _offerId, 'The offer must exist');
-    require(_offer.user == msg.sender, 'The offer can only be canceled by the owner');
-    require(_offer.fulfilled == false, 'A fulfilled offer cannot be cancelled');
-    require(_offer.cancelled == false, 'An offer cannot be cancelled twice');
+  function cancelOffer(uint _offerId) public canCancelOffer(_offerId){
+    _Offer storage _offer = offers[_offerId];    
     nftCollection.transferFrom(address(this), msg.sender, _offer.id);
     _offer.cancelled = true;
     emit OfferCancelled(_offerId, _offer.id, msg.sender);
@@ -68,13 +59,33 @@ contract NFTMarketplace {
 
   function claimFunds() public {
     require(userFunds[msg.sender] > 0, 'This user has no funds to be claimed');
-    payable(msg.sender).transfer(userFunds[msg.sender]);
-    emit ClaimFunds(msg.sender, userFunds[msg.sender]);
-    userFunds[msg.sender] = 0;    
+	userFunds[msg.sender] = 0; // preventing repeated fund transfer through reentrancy attack by making the amount 0 
+   	(bool sent, ) = payable(msg.sender).call{value: userFunds[msg.sender]}(""); //use the latest recommended method to transfer ether 
+    require(sent, "Bid withdrawal failed");
+    emit ClaimFunds(msg.sender, userFunds[msg.sender]);        
   }
 
   // Fallback: reverts if Ether is sent to this smart-contract by mistake
   fallback () external {
     revert();
+  }
+
+  modifier canFillOffer(uint _offerId) {
+        _Offer storage _offer = offers[_offerId];  
+        require(_offer.offerId == _offerId, 'The offer must exist');
+        require(_offer.user != msg.sender, 'The owner of the offer cannot fill it');
+        require(!_offer.fulfilled, 'An offer cannot be fulfilled twice');
+        require(!_offer.cancelled, 'A cancelled offer cannot be fulfilled');
+        require(msg.value == _offer.price, 'The ETH amount should match with the NFT Price');
+        _;
+  }
+
+  modifier canCancelOffer(uint _offerId) {
+        _Offer storage _offer = offers[_offerId];  
+        require(_offer.offerId == _offerId, 'The offer must exist');
+        require(_offer.user == msg.sender, 'The offer can only be canceled by the owner');
+        require(_offer.fulfilled == false, 'A fulfilled offer cannot be cancelled');
+        require(_offer.cancelled == false, 'An offer cannot be cancelled twice');
+        _;
   }
 }
